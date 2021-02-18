a8075b6aed75fb845e8319c028c5001bd845160f
gcl1991 <737720583@qq.com>
2021-02-18 17:08:53 +0800

    update

--- proficiency-spring4x/chapter2/src/main/java/com/smart/web/LoginController.java
+++ proficiency-spring4x/chapter2/src/main/java/com/smart/web/LoginController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,6 +21,7 @@ public class LoginController {
     public String loginPage() {
         return "login";
     }
+
 
     // 请求参数会按参数名字默认契约绑定到LoginCommand对象
     @RequestMapping(value = "/loginCheck.html")
